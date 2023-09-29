94199fb9e13a766e0a3de79dae59f400567bba3d
maheshnannepaga <maheshnannepaga@gmail.com>
2023-09-29 15:45:53 -0500

    Update hp_4.py

--- src/hp_4.py
+++ src/hp_4.py
@@ -28,19 +28,16 @@ def date_range(start, n):
             added_list.append(datetime.strptime(start, "%Y-%m-%d") + timedelta(days=inc))
         return added_list
 
+    def add_date_range(values, start_date):
+        if not isinstance(start_date, str):
+            raise TypeError("start_date must be a string")
 
-def add_date_range(values, start_date):
-    """Adds a daily date range to the list `values` beginning with
-    `start_date`.  The date, value pairs are returned as tuples
-    in the returned list."""
-    if not isinstance(start, str):
-        raise TypeError
-    elif not isinstance(n, int):
-        raise TypeError
-    else:
         added_list = []
-        for inc in range(0, n):
-            added_list.append(datetime.strptime(start, "%Y-%m-%d") + timedelta(days=inc))
+        date_objects = date_range(start_date, len(values))
+
+        for date, value in zip(date_objects, values):
+            added_list.append((date, value))
+
         return added_list
 
 
@@ -90,7 +87,7 @@ def fees_report(infile, outfile):
 # under the data directory.
 
 if __name__ == '__main__':
-    
+
     try:
         from src.util import get_data_file_path
     except ImportError:
